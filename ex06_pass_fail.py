84c93665d57a012e9e31faba7f00278e0e18619b
Ravishasthri <sravishasthri@gmail.com>
2026-04-20 19:50:05 +0800

    more practice added

---
+++ ex06_pass_fail.py
@@ -0,0 +1,5 @@
+marks = input("Enter your marks: ")
+if int(marks) >= 50:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
---
+++ ex07_grade_calculator.py
@@ -0,0 +1,9 @@
+marks = input("Enter your marks: ")
+if int(marks) >= 75:
+    print("Grade: A")
+elif int(marks) >= 65:
+    print("Grade: B")
+elif int(marks) >= 50:  
+    print("Grade: C")
+else:
+    print("Grade: F")       
